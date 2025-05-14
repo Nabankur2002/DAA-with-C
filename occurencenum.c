a6858dd3a7eee38d24e87a9cdc167bc93ca4ec92
Nabankur Sarkar <nabankursarkar89@gmail.com>
2025-05-15 03:13:34 +0530

    Add files via upload

---
+++ occurencenum.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int countfreq(int arr[], int size, int x)
+{
+    int count = 0;
+    int low = 0, high = size - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == x)
+        {
+            count = 1;
+            int left = mid - 1;
+            int right = mid + 1;
+            while (left >= 0 && arr[left] == x)
+            {
+                count++;
+                left--;
+            }
+            while (right < size && arr[right] == x)
+            {
+                count++;
+                right++;
+            }
+            return count;
+        }
+        else if (arr[mid]<x)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int size, x;
+    printf("Enter the size of the array: ");
+    scanf("%d ", &size);
+
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter the target element to be counted ");
+    scanf("%d", &x);
+    int count = countfreq(arr, size, x);
+    printf("Occurence if the target is: %d", count);
+    return 0;
+}
