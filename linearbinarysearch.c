16b3932e26a8288eb001aa01961b6f04237da581
Nabankur Sarkar <nabankursarkar89@gmail.com>
2025-06-12 11:32:33 +0530

    linearbinarysearch.c

--- linearbinarysearch.c
+++ linearbinarysearch.c
@@ -1,90 +1,88 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define SIZE 10
+#include <stdio.h>
+#include <stdlib.h>
 
-// Global array
-int arr[SIZE]={-100,-89,-56, -5,0,6,10,75, 99};
+// Function prototypes
+void sortArray(int arr[], int size);
+int linearSearch(int arr[], int size, int key, int *comparisons);
+int binarySearch(int arr[], int size, int key, int *comparisons);
 
-//Function prototypes
-void sortArray();
-int linearSearch(int key, int *comparisons);
-int binarySearch(int key, int *comparisons);
-
-//Here we use Bubble Sort technique
-void sortArray() {
-    for(int i=0; i<SIZE-1; i++){ // passes
-        for(int j=0; j<SIZE-i-1; j++){ //comparisons
-            if(arr[j]<arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+// Bubble Sort (ascending)
+void sortArray(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-int linearSearch(int key, int *comparisons){
-    for (int i = 0; i < SIZE-1; i++)
-    {
+int linearSearch(int arr[], int size, int key, int *comparisons) {
+    for (int i = 0; i < size; i++) {
         (*comparisons)++;
-        if(arr[i]==key){
+        if (arr[i] == key) {
             return i;
         }
     }
     return -1;
 }
 
-int binarySearch(int key, int *comparisons){
-    int start=0; 
-    int end=SIZE -1;
-    int mid;
-    while (start<=end)
-    {
+int binarySearch(int arr[], int size, int key, int *comparisons) {
+    int start = 0, end = size - 1, mid;
+    while (start <= end) {
         (*comparisons)++;
-        mid=start+(end-start)/2;
-        if(arr[mid]==key){
+        mid = start + (end - start) / 2;
+        if (arr[mid] == key) {
             return mid;
-        }
-        else if(arr[mid]>key){
-            start=mid+1;
-        }
-        else{
-            end=mid-1;
+        } else if (arr[mid] > key) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
         }
     }
     return -1;
 }
 
-int main(){
-    int key, liComparisons=0, biComaprisons=0;
+int main() {
+    int size, key, liComparisons = 0, biComparisons = 0;
+
+    printf("Enter the size of array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("Enter the array elements: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
     printf("Enter the integer to search for: ");
     scanf("%d", &key);
 
-    int linearResult= linearSearch(key, &liComparisons);
-    
-    if(linearResult!=-1){
-        printf("In linear Search the key found at index %d after %d comparisons\n", linearResult, liComparisons);
-    }
-    else{
-        printf("The key is not found with %d comparisons\n", liComparisons);
+    int linearResult = linearSearch(arr, size, key, &liComparisons);
+    if (linearResult != -1) {
+        printf("In Linear Search, the key found at index %d after %d comparisons\n", linearResult, liComparisons);
+    } else {
+        printf("Linear Search: Key not found after %d comparisons\n", liComparisons);
     }
 
-    sortArray();
-    int binaryResult=binarySearch(key, &biComaprisons);
-        if(binaryResult!=-1){
-            printf("In binary Search the key found at index %d after %d comparisons\n", binaryResult, biComaprisons);
-        }
-        else{
-            printf("The key is not found with %d comparisons\n", biComaprisons);
-        }
-    
-    
-    //Condition to find which one is good
-    if(liComparisons<biComaprisons){
-        printf("Linear Search is good\n");
+    sortArray(arr, size);
+
+    int binaryResult = binarySearch(arr, size, key, &biComparisons);
+    if (binaryResult != -1) {
+        printf("In Binary Search, the key found at index %d after %d comparisons\n", binaryResult, biComparisons);
+    } else {
+        printf("Binary Search: Key not found after %d comparisons\n", biComparisons);
     }
-    else{
-        printf("Binary Search is good\n");
+
+    // Determine better search
+    if (liComparisons < biComparisons) {
+        printf("Linear Search is better in this case.\n");
+    } else {
+        printf("Binary Search is better in this case.\n");
     }
-    return -1;
+
+    return 0;
 }
